3e6b856d0c7730b4fca9459a7350f17577e9d050
almighty-yawnn <herbertmantey@gmail.com>
2023-08-08 16:57:16 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-skip = {'e', 'q'}
-skipped = ''
 for Ascii in range(97, 123):
-    if chr(Ascii) not in skip:
-        skipped += chr(Ascii)
-print(skipped, end='')
+    if chr(Ascii) not in ('e', 'q'):
+        print("{}".format(chr(Ascii)), end="")
