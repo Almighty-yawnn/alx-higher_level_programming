2206f20a1481bcb626b784d715c39979b2ec03a6
almighty-yawnn <herbertmantey@gmail.com>
2023-08-10 19:26:15 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-import sys
-def pr_args():
-    argv2 = sys.argv[1:]
-    num_args = len(argv2)
-    
-    if num_args == 0:
-        print(f"{num_args} arguments.")
-        return
-    elif num_args == 1:
-        print(f"{num_args} argument:")
-    else:
-        print(f"{num_args} arguments:")
-    for i in range(num_args):
-        print(f"{i + 1}: {argv2[i]}")
 if __name__ == "__main__":
+    import sys
+    def pr_args():
+        argv2 = sys.argv[1:]
+        num_args = len(argv2)
+
+        if num_args == 0:
+            print(f"{num_args} arguments.")
+            return
+        elif num_args == 1:
+            print(f"{num_args} argument:")
+        else:
+            print(f"{num_args} arguments:")
+        for i in range(num_args):
+            print(f"{i + 1}: {argv2[i]}")
     pr_args()
