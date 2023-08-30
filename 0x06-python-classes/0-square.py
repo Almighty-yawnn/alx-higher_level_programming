2e37c6c38fe106638e7f28545ab8bbb3f001e98c
almighty-yawnn <herbertmantey@gmail.com>
2023-08-29 18:10:21 -0700

    0. My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+0. My first square
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+    pass
