e386ba56f3f111cf9c34a4f2743b3b9a7e4a9f93
almighty-yawnn <herbertmantey@gmail.com>
2023-09-04 18:05:24 -0700

    0x08-python-more_classes 0-rectangle.py"

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+"""
+0-rectangle.py
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
