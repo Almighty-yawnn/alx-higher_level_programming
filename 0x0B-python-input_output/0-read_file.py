6a183afdd675efe1808e0a538c00a8559b55e7d6
almighty-yawnn <herbertmantey@gmail.com>
2023-09-12 16:31:19 -0700

    my_file_0.txt

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+0-read_file.py
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads the file
+
+    Arguments:
+        filename (str): The name of the file to open
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
